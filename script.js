6920944e7d1408cb38ba10933315529cb0ae5dcb
Edwin Hernandez <edwinhernandez80@icloud.com>
2022-12-31 12:09:36 -0800

    added toggle between high score and questions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+
+var questionwindow = document.querySelector("#questionwindow");
 var startbutton = document.querySelector("#startbutton");
 var timerEl = document.querySelector("#countdown");
 var question = document.querySelector("#question");
@@ -8,36 +10,61 @@ var mcC = document.querySelector("#mcC");
 var mcD = document.querySelector("#mcD");
 var starttext = document.querySelector("#starttext");
 var correctanswer = document.querySelector("#correctanswer");
+var validationtext = document.querySelector("#validationtext");
+var questionnumber = 1;
+var timeleft = 160;
+var highscorebutton = document.querySelector("#highscorebutton");
+var Highscores = document.querySelector("#Highscores");
+
+highscorebutton.addEventListener("click",function(event){
+    if (Highscores.style.display == "block"){
+        questionwindow.style.display="block";
+        Highscores.style.display= "none";
+        
+    }else{
+    questionwindow.style.display="none";
+    Highscores.style.display= "block";
+    }
+
+});
+
 
 mcA.addEventListener("click",function(event){validation(event)});
-
+mcB.addEventListener("click",function(event){validation(event)});
+mcC.addEventListener("click",function(event){validation(event)});
+mcD.addEventListener("click",function(event){validation(event)});
 
 
 startbutton.addEventListener("click",function(event)
 {countdown();
-
-var questionlist = questions(1);
-mcA.textContent = questionlist[0].answers[0];
-mcB.textContent = questionlist[1].answers[1];
-mcC.textContent = questionlist[2].answers[2];
-mcD.textContent = questionlist[3].answers[3];
-question.textContent = questionlist[0].questions;
-console.log(questionlist[0].questions);
-starttext.textContent = "";
-correctanswer.textContent = questionlist[0].correctanswer;
-
-
-
+populatequestiondata(questionnumber);   
 
 });
 
+function populatequestiondata (questionnumber) {
+ 
+    var questionlist = questions(questionnumber);
+    mcA.textContent = questionlist[0].answers[0];
+    mcB.textContent = questionlist[0].answers[1];
+    mcC.textContent = questionlist[0].answers[2];
+    mcD.textContent = questionlist[0].answers[3];
+    question.textContent = questionlist[0].questions;
+    starttext.textContent = "";
+    correctanswer.textContent = questionlist[0].correctanswer;
+} 
+
+
+
+
+
+
 function countdown() {
-    var timeLeft = 160;
-  
+    
+    
     var timeInterval = setInterval(function () {
-      timeLeft--;
-      timerEl.textContent = timeLeft;
-      if(timeLeft === 0){
+      timeleft--;
+      timerEl.textContent = timeleft;
+      if(timeleft === 0){
         timerEl.textContent = "";
         clearInterval(timeInterval)
         displayMessage();
@@ -74,8 +101,17 @@ function countdown() {
     if (event) {
         var useranswer = event.target.textContent;
     if (useranswer == correctanswer.textContent) {
-        
+        questionnumber++;
+        validationtext.textContent = "";
+        populatequestiondata(questionnumber);
+    
+    }else {
+        validationtext.textContent = "wrong";
+        timeleft = timerEl.textContent - 10
+         countdown();
     }  
+
+    
     
     }
     
